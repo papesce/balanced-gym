76fed3872818c6463080aaeee0dcdd854ec54ece
Pablo Pesce <papesce@gmail.com>
2017-12-22 18:33:26 -0300

    addexercise icon

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,28 +1,54 @@
 import * as React from "react";
-import { Navbar, NavbarBrand, Container, Row, Col } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  Container,
+  Row,
+  Col,
+  NavLink
+} from "reactstrap";
 import { ExerciseListC } from "../container/ExerciseListC";
 import { MuscleGroupC } from "../container/MuscleGroupC";
 import "./Home.css";
-import  sizeMe from "react-sizeme";
+import sizeMe from "react-sizeme";
 
-class HomeC extends React.Component {
+interface HomeCProps {}
+
+interface HomeCState {
+  collapsed: boolean;
+}
+
+class HomeC extends React.Component<HomeCProps, HomeCState> {
+  constructor(props: HomeCProps) {
+    super(props);
+    this.state = { collapsed: true };
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+  }
+  toggleNavbar = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  }
   render() {
     return (
       <div>
-         <Navbar dark={true} color="dark" >
+        <Navbar dark={true} color="dark">
           <NavbarBrand href="/">Balanced Gym</NavbarBrand>
+          <NavLink className="navbar-toggler" href="/addExercise">
+            <span
+              className={"navbar-toggler-plus fa fa-plus"}
+            />
+          </NavLink>
         </Navbar>
-          <Container className="app-container">
-            <Row className="app-row">
-              <Col xs={10} md={4}>
-               <h3 className={"title"}>Exercises</h3>
-              </Col>
-              <Col xs={10} md={6}>
-                <MuscleGroupC/>
-              </Col>
-              </Row>
-          </Container>
-          <ExerciseListC/>
+        <Container className="app-container">
+          <Row className="app-row">
+            <Col xs={10} md={4}>
+              <h3 className={"title"}>Exercises</h3>
+            </Col>
+            <Col xs={10} md={6}>
+              <MuscleGroupC />
+            </Col>
+          </Row>
+        </Container>
+        <ExerciseListC />
       </div>
     );
   }
