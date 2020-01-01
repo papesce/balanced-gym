c7613bdf4a460fbe2005a86396b4f79c5cf13f40
Pablo Pesce <pablo.pesce@ibm.com>
2020-01-01 13:20:24 -0300

    enable patch in server

--- server/src/server.js
+++ server/src/server.js
@@ -37,7 +37,7 @@ mongoose.Promise = Promise;
 // Enable cors for dev
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE");
   // res.header("Access-Control-Allow-Headers",
   // "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
