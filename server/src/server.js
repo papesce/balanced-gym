11131ec6aed93a7257127ab5b804f933fa8a0ff6
Pablo Pesce <papesce@gmail.com>
2020-02-09 08:34:09 -0300

    backup Feb 9

--- server/src/server.js
+++ server/src/server.js
@@ -68,7 +68,7 @@ app.use(
   })
 );
 
-console.log(path.join(__dirname, "./assets"));
+// console.log(path.join(__dirname, "./assets"));
 app.use(express.static(path.join(__dirname, "./assets")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
